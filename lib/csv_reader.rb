ae6a4bf2282e6a1d9dab735811017429496f93bb
Matthew Landauer <matthew@oaf.org.au>
2015-05-16 14:57:05 +1000

    Move method to its own class

---
+++ lib/csv_reader.rb
@@ -0,0 +1,22 @@
+class CsvReader
+  # Loads a csv and returns a hash for each line
+  # Assumes the first line is the name of the column
+  def self.load_csv(file)
+    line_count = 0
+    headers = nil
+    CSV.foreach(file) do |row|
+      headers = row if line_count == 0
+      if line_count > 0
+        # Ignore lines with the wrong number of entries
+        if headers.count == row.count
+          hash = {}
+          row.each_with_index do |value,i|
+            hash[headers[i]] = value
+          end
+          yield hash
+        end
+      end
+      line_count += 1
+    end
+  end
+end
