fc75c79d8ccae20c1bd018091db11ea19b659d88
Matthew Landauer <matthew@oaf.org.au>
2015-05-16 13:18:18 +1000

    Show the first twenty lines

--- show.rb
+++ show.rb
@@ -12,6 +12,7 @@ end
 
 line_count = 0
 headers = nil
+display_line([["Transaction", 11], ["Date", 10], ["Stock Code", 10], ["Desc", 25], ["Qty", 10], ["Total (ex gst)", 15], ["Total (inc gst)", 15]])
 CSV.foreach("data/Kat Szum sales summary.csv") do |row|
   headers = row if line_count == 0
   if line_count > 0
@@ -22,8 +23,9 @@ CSV.foreach("data/Kat Szum sales summary.csv") do |row|
       hash[headers[i]] = value
     end
     #p hash
-    display_line([["Transaction", 10], ["Date", 10], ["Stock Code", 10], ["Desc", 25], ["Qty", 10], ["Total (ex gst)", 15], ["Total (inc gst)", 15]])
-    display_line([[hash["TransRefCode"], 10], [hash["TransDate"], 10], [hash["StockCode"], 10], [hash["StockDesc"], 25], [hash["LineQty"], 10], [hash["LineTotalExGST"], 15], [hash["LineTotalIncGST"], 15]])
+    display_line([[hash["TransRefCode"], 11], [hash["TransDate"], 10], [hash["StockCode"], 10], [hash["StockDesc"], 25], [hash["LineQty"], 10], [hash["LineTotalExGST"], 15], [hash["LineTotalIncGST"], 15]])
+  end
+  if line_count > 20
     exit
   end
   line_count += 1
