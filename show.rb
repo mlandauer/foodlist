1158527cd424e520f9d4b8ced7fce547deea412a
Matthew Landauer <matthew@oaf.org.au>
2015-05-16 13:16:06 +1000

    Show the first line of data in a more readable form

--- show.rb
+++ show.rb
@@ -2,6 +2,14 @@
 
 require "csv"
 
+def display_line(array)
+  line = ""
+  array.each do |item, width|
+    line += item[0..width-1].ljust(width) + " "
+  end
+  puts line
+end
+
 line_count = 0
 headers = nil
 CSV.foreach("data/Kat Szum sales summary.csv") do |row|
@@ -13,7 +21,9 @@ CSV.foreach("data/Kat Szum sales summary.csv") do |row|
     row.each_with_index do |value,i|
       hash[headers[i]] = value
     end
-    p hash
+    #p hash
+    display_line([["Transaction", 10], ["Date", 10], ["Stock Code", 10], ["Desc", 25], ["Qty", 10], ["Total (ex gst)", 15], ["Total (inc gst)", 15]])
+    display_line([[hash["TransRefCode"], 10], [hash["TransDate"], 10], [hash["StockCode"], 10], [hash["StockDesc"], 25], [hash["LineQty"], 10], [hash["LineTotalExGST"], 15], [hash["LineTotalIncGST"], 15]])
     exit
   end
   line_count += 1
