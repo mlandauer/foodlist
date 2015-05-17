bce2011e20fc58e1cf29db3c7507c8d8cd51613e
Matthew Landauer <matthew@oaf.org.au>
2015-05-17 12:29:27 +1000

    Add account model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150516235630) do
+ActiveRecord::Schema.define(version: 20150517022826) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "ref"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "line_items", force: :cascade do |t|
     t.integer  "receipt_id"
