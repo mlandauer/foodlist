58b30d3e304ae1ceac18d968512d3b41f96747ec
Matthew Landauer <matthew@oaf.org.au>
2015-05-17 15:44:11 +1000

    Remove comment

--- db/migrate/20150516042849_create_transactions.rb
+++ db/migrate/20150516042849_create_transactions.rb
@@ -3,7 +3,6 @@ class CreateTransactions < ActiveRecord::Migration
     create_table :receipts do |t|
       t.string :code
       t.date :date
-      #t.references :account, index: true, foreign_key: true
 
       t.timestamps null: false
     end
