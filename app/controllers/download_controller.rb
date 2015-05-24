817c729832a8d8e6ab62f04cc1b1f852f0385a3d
Matthew Landauer <matthew@oaf.org.au>
2015-05-24 13:32:15 +1000

    Download all the data

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -1,4 +1,20 @@
+require "csv"
+
 class DownloadController < ApplicationController
   def index
+  end
+
+  def data
+    # Create a csv
+    account = current_user.account
+    csv_string = CSV.generate do |csv|
+      csv << ["Date (YYYY-MM-DD)", "Receipt code", "Product stock code", "Product description", "Quantity", "Total including GST"]
+      account.receipts.order(date: :desc).find_each do |receipt|
+        receipt.line_items.find_each do |l|
+          csv << [receipt.date, receipt.code, l.product.stock_code, l.product.description, l.quantity, l.total_inc_gst]
+        end
+      end
+    end
+    send_data csv_string, :filename => "receipts.csv"
   end
 end
