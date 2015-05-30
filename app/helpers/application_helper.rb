3344f1f7d763572957ba8f8cef73129ae4dae2e0
Matthew Landauer <matthew@oaf.org.au>
2015-05-30 11:58:37 +1000

    Show roughly how often you buy a product

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def rough_period_of_time_in_words(days)
+    if days >= 7
+      weeks = days / 7.0
+      if weeks.round >= 4
+        months = weeks / 4.0
+        pluralize(months.round, 'month')
+      else
+        pluralize(weeks.round, 'week')
+      end
+    else
+      pluralize(days, 'day')
+    end
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -9,4 +9,13 @@ class Product < ActiveRecord::Base
   def total_inc_gst(account)
     line_items(account).sum(:total_inc_gst)
   end
+
+  def average_time_between_purchases(account)
+    # TODO: We should really base this on the earliest and latest time of all
+    # the receipts
+    earliest_time = account.receipts.minimum(:date)
+    latest_time = account.receipts.maximum(:date)
+    number = line_items(account).count
+    ((latest_time - earliest_time) / number).round
+  end
 end
