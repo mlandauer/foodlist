180f670bc8a8df1f02054c79db76f8c9967a9b61
Matthew Landauer <matthew@oaf.org.au>
2015-05-30 12:43:12 +1000

    Rename method

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,12 +2,12 @@ class Product < ActiveRecord::Base
   extend FriendlyId
   friendly_id :stock_code
 
-  def line_items(account)
+  def line_items_for_account(account)
     LineItem.joins(:receipt).where(product: self, receipts: {account_id: account.id})
   end
 
   def total_inc_gst(account)
-    line_items(account).sum(:total_inc_gst)
+    line_items_for_account(account).sum(:total_inc_gst)
   end
 
   def average_time_between_purchases(account)
@@ -15,7 +15,7 @@ class Product < ActiveRecord::Base
     # the receipts
     earliest_time = account.receipts.minimum(:date)
     latest_time = account.receipts.maximum(:date)
-    number = line_items(account).count
+    number = line_items_for_account(account).count
     ((latest_time - earliest_time) / number).round
   end
 end
