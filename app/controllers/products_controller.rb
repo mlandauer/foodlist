67a8664c0603198448aad42ae7ac4f7b1c87c813
Matthew Landauer <matthew@oaf.org.au>
2015-05-30 13:39:04 +1000

    Order products by how often we buy them

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,14 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.where("stock_code != ''").order("CAST(stock_code AS int)").all
+    #@products = Product.where("stock_code != ''").order("CAST(stock_code AS int)").all
+    @products = Product
+      .where("stock_code != ''")
+      .joins(line_items: :receipt)
+      .where(receipts: { account_id: current_user.account.id })
+      .group('products.id')
+      .select('products.*, count(*) as count_all')
+      .order('count_all desc')
+      .all
   end
 
   def show
